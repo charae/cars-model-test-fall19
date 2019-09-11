a843a3f976ef5dcc84dce6425d3e72dbe6711900
first last <charaexo@icloud.com>
2019-09-10 19:03:22 -0600

    tested attr valid

---
+++ app/models/car.rb
@@ -0,0 +1,36 @@
+class Car < ApplicationRecord
+  # return all cars asc order by model
+  def self.by_model
+
+end
+
+  # return all cars asc order by make
+def self.by_make
+
+
+end
+
+# no params pass return asc all the car by mileage
+# :desc order by mileage
+def self.by_mileage(direction = :asc)
+
+end
+
+# change the color of the car with the param
+def paint(color)
+
+
+end
+
+# return a hash of all the car's info
+def info
+
+end
+
+# returns a string of the car horn
+def honk
+  
+end
+
+
+end
